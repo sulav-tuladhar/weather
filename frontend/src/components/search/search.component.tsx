bad490d054077c007b1f074e68c31783c981be81
sulav tuladhar <sulavtuladhar@ramailo.tech>
2024-05-26 15:50:27 +0545

    react and tailwind css initialized

---
+++ frontend/src/components/search/search.component.tsx
@@ -0,0 +1,7 @@
+function SearchComponent() {
+    return (
+        <input type='text' placeholder='Please enter city name' className="p-4 w-[20rem] outline-none rounded-full"/>
+    )
+}
+
+export default SearchComponent
