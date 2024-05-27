b0aaf9bd998cb2f86b4b321280b453aef8d91163
sulav tuladhar <sulavtuladhar@ramailo.tech>
2024-05-27 13:16:59 +0545

    fix:database createdAt column added

--- backend/src/entities/weather.entity.ts
+++ backend/src/entities/weather.entity.ts
@@ -34,5 +34,7 @@ export class Weather {
 
     @Column()
     humidity!: number
-    
+
+    @Column()
+    createdAt!: string;
 }
--- backend/src/modules/weather/weather.controller.ts
+++ backend/src/modules/weather/weather.controller.ts
@@ -6,22 +6,27 @@ import api_error from "../../helpers/api_error";
 import { Weather } from "../../entities/weather.entity";
 import map_weather_req from "../../helpers/map_weather_req";
 import { repo } from "../../helpers/repo";
+import moment from "moment";
 
 export const getWeatherInfo = async(req: Request, res: Response, next: NextFunction) => {
     try{
         const searchParams = req.query.q;
         // const data = await axios.get(`${process.env.BASE_URL}q=${searchParams}`)
-        const data = await GET(`${process.env.BASE_URL}q=${searchParams}`)
-        const weatherInfo = data.data;      
-        if(!weatherInfo){
-            throw customError("Weather data not found", 404)
-        }
-        var newWeatherInfo = new Weather();
-        const mapped_data = map_weather_req(newWeatherInfo, weatherInfo);
-        const savedData = await repo.weatherRepo.save(mapped_data);
-        res.status(200).json({
-            data: savedData,
-            status: 200
+        // const data = await GET(`${process.env.BASE_URL}q=${searchParams}`)
+        // const weatherInfo = data.data;      
+        // if(!weatherInfo){
+        //     throw customError("Weather data not found", 404)
+        // }
+        // var newWeatherInfo = new Weather();
+        // weatherInfo.createdAt = moment().format("YYYY/MM/DD");
+        // const mapped_data = map_weather_req(newWeatherInfo, weatherInfo);
+        // const savedData = await repo.weatherRepo.save(mapped_data);
+        // res.status(200).json({
+        //     data: savedData,
+        //     status: 200
+        // })
+        res.json({
+            message: "Hello"
         })
     }catch(err: any){
         console.log(err.response.data.error)
